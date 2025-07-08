a3de8c8db07a3dec5eee1e07d50f1401e9f06b51
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-07-08 16:54:34 +0300

    add second dictionary comprehension example with different range and step

--- workbench/dictionary_comprehension_example_1.py
+++ workbench/dictionary_comprehension_example_1.py
@@ -11,4 +11,9 @@ def generate_dictionary_one_liner() -> dict:
     dictionary = {f"{i}": f"{"FizzBuzz" if i%5==0 else "Fizz"}" for i in range(1,201) if i%3==0 and i%4==0}
     return dictionary
 
+def generate_dictionary_one_liner_2() -> dict:
+    dictionary = {f"{i}": f"{"FizzBuzz" if i%5==0 else "Fizz"}" for i in range(0,201,12)}
+    return dictionary
+
 pprint(generate_dictionary_one_liner())
+pprint(generate_dictionary_one_liner_2())
