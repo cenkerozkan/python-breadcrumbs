98d4dca1d6ac69c16cd99506e404aaa0f5f754a4
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-08-24 01:36:14 +0300

    feat: update FizzBuzz dictionary comprehension and rename test file

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -1,3 +1,5 @@
-fizz_buzz_dict = {number: "FizzBuzz" if number%5 == 0 else "Fizz" for number in range(0,201,12)}
+fizz_buzz_dict_with_step = {number: "FizzBuzz" if number % 5 == 0 else "Fizz" for number in range(0, 201, 12)}
+fizz_buzz_dict_without_step = {number: "FizzBuzz" if number%5 == 0 else "Fizz" for number in range(0,201,12)
+                               if number % 3 == 0 and number % 4 == 0}
 
-print(fizz_buzz_dict)
+print(fizz_buzz_dict_with_step)
