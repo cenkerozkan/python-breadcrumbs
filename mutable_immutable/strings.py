621d6b255f672ff784f23543f71d3810a50d22d7
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-08-27 00:27:25 +0300

    feat: rename init files and add asynchronous sleep examples

--- mutable_immutable/strings.py
+++
@@ -1,5 +0,0 @@
-number: int = 45
-print(id(number))
-
-number += 1
-print(id(number))
--- workbench/multiple_list_example.py
+++
@@ -1,5 +0,0 @@
-sample_input_1:list[list[int]] = [[1,2], [3,4]]
-sample_input_2:list[list[int]] = [[1, 2, 2.3, 4, 5]] + [[3, 7, 3, 8, 15]] # Also with a plus
-sample_input_3:list[list[int]] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-def
